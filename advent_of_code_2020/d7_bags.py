9831a18bcee37a730065d197033543645751b7e7
merengue <therealmerengue@gmail.com>
2020-12-07 21:21:19 +0100

    AoC day 7 part 1.

--- advent_of_code_2020/d7_bags.py
+++ advent_of_code_2020/d7_bags.py
@@ -1,53 +1,60 @@
-from typing import List
+from typing import Dict, Set
 
 from util.files import stripped_input_lines_from
 
 
-class Content:
-    def __init__(self, color: str, amount: int) -> None:
+class Bag:
+    def __init__(self, color: str, possible_contents: Dict[str, int]) -> None:
         self._color = color
-        self._amount = amount
+        self._possible_contents = possible_contents
 
     @property
-    def amount(self) -> int:
-        return self._amount
+    def possible_contents(self) -> Dict[str, int]:
+        return self._possible_contents
 
     @property
     def color(self) -> str:
         return self._color
 
 
-class Bag:
-    def __init__(self, color: str, possible_contents: List[Content]) -> None:
-        self._color = color
-        self._possible_contents = possible_contents
-
-    @property
-    def possible_contents(self):
-        return self._possible_contents
-
-
-def read_bags_from(path: str) -> List[Bag]:
+def read_bags_from(path: str) -> Dict[str, Bag]:
     lines = stripped_input_lines_from(path)
-    bags: List[Bag] = []
+    bags: Dict[str, Bag] = {}
     for line in lines:
         words = line.split(' ')
         bag_color = words[0] + ' ' + words[1]
         word_index = 4
-        contents: List[Content] = []
-        while word_index < len(line):
+        contents: Dict[str, int] = {}
+        while word_index < len(words):
+            if not words[word_index].isdigit():
+                break
             amount = int(words[word_index])
             content_color = words[word_index + 1] + ' ' + words[word_index + 2]
-            contents.append(Content(content_color, amount))
+            contents[content_color] = amount
             word_index += 4
-        bags.append(Bag(bag_color, contents))
+        bags[bag_color] = Bag(bag_color, contents)
     return bags
 
 
-def build_indirect_containers_for(color: str, bags: List[Bag], containers: List[Bag]) -> int:
-    if not bags:
-        return containers
+def build_indirect_containers_for(color: str, all_bags: Dict[str, Bag], current_containers: Set[str]):
+    next_colors_to_check = []
+    for bag in all_bags.values():
+        if bag.color == color:
+            continue
+        if color in bag.possible_contents.keys() and bag.color not in current_containers:
+            current_containers.add(bag.color)
+            next_colors_to_check.append(bag.color)
+
+    for color_to_check in next_colors_to_check:
+        build_indirect_containers_for(color_to_check, all_bags, current_containers)
 
 
-def number_of_indirect_containers_for(color: str, bags: List[Bag]) -> int:
-    return build_indirect_containers_for(color, bags, [])
+def number_of_indirect_containers_for(color: str, bags: Dict[str, Bag]) -> int:
+    containers: Set[str] = set()
+    build_indirect_containers_for(color, bags, containers)
+    return len(containers)
+
+
+if __name__ == '__main__':
+    input_bags = read_bags_from('d7_bags_input.txt')
+    print(number_of_indirect_containers_for('shiny gold', input_bags))
