187dcc19d26ca7e23e79edc963dd276b6f3f38df
merengue <therealmerengue@gmail.com>
2020-12-23 21:50:45 +0100

    AoC day 23 - part 2 cd.

--- advent_of_code_2020/d23_cups.py
+++ advent_of_code_2020/d23_cups.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Set
 
 
 class Cup:
@@ -10,33 +10,41 @@ class Cup:
     def value(self) -> int:
         return self._value
 
-    @next.setter
-    def next(self, next_cup):
-        self._next_cup = next_cup
-
     @property
-    def next(self, next_cup):
+    def next_cup(self) -> 'Cup':
         return self._next_cup
 
+    @next_cup.setter
+    def next_cup(self, value):
+        self._next_cup = value
 
-class CupsList:
-    def __init__(self, head: Cup) -> None:
-        self._head = head
 
-    @property
-    def head(self) -> Cup:
-        return self._head
+def wrap_index(index: int, number_of_cups: int) -> int:
+    return index % number_of_cups
+
+
+def wrap_destination_cup(cup: int, number_of_cups: int) -> int:
+    return cup if cup > 0 else number_of_cups
 
 
 def simulate_game(initial_order: str, number_of_moves: int, number_of_cups: int = -1) -> str:
     assert initial_order.isdigit()
     initial_len = len(initial_order)
+    assert initial_len > 1 or number_of_cups > 1
+    number_of_cups = initial_len if initial_len > number_of_cups else number_of_cups
 
-    def wrap_destination_cup(cup: int) -> int:
-        return cup if cup > 0 else max(initial_len, number_of_cups)
-
-    def wrap_index(index: int) -> int:
-        return index % len(cups)
+    all_cups: Set[Cup] = set()
+    head = None
+    tail = None
+    for cup_str in initial_order:
+        cup = Cup(int(cup_str))
+        all_cups.add(cup)
+        if head is None:
+            head = cup
+        if tail is not None:
+            tail.next_cup = cup
+        tail = cup
+    tail.next_cup = head
 
     cups = [int(cup) for cup in initial_order]
     if number_of_cups > initial_len:
@@ -48,14 +56,14 @@ def simulate_game(initial_order: str, number_of_moves: int, number_of_cups: int 
     current_cup = cups[current_cup_index]
     while move < number_of_moves:
         picked_up_cups = []
-        cup_to_pick_up_index = wrap_index(current_cup_index + 1)
+        cup_to_pick_up_index = wrap_index(current_cup_index + 1, len(cups))
         while len(picked_up_cups) < 3:
             if cup_to_pick_up_index == len(cups):
                 cup_to_pick_up_index = 0
             picked_up_cups.append(cups.pop(cup_to_pick_up_index))
-        destination_cup = wrap_destination_cup(current_cup - 1)
+        destination_cup = wrap_destination_cup(current_cup - 1, number_of_cups)
         while destination_cup in picked_up_cups:
-            destination_cup = wrap_destination_cup(destination_cup - 1)
+            destination_cup = wrap_destination_cup(destination_cup - 1, number_of_cups)
         destination_index = cups.index(destination_cup) + 1
         if destination_index == len(cups):
             for cup in picked_up_cups:
@@ -63,7 +71,7 @@ def simulate_game(initial_order: str, number_of_moves: int, number_of_cups: int 
         else:
             for cup in reversed(picked_up_cups):
                 cups.insert(destination_index, cup)
-        current_cup_index = wrap_index(cups.index(current_cup) + 1)
+        current_cup_index = wrap_index(cups.index(current_cup) + 1, len(cups))
         current_cup = cups[current_cup_index]
         move += 1
 
