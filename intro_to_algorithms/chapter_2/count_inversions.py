7e7185dc3ae0dab755b8430123a10c098e9e85b3
Mere Ngue <therealmerengue@gmail.com>
2021-01-24 16:57:30 +0100

    simplified count_inversions

--- intro_to_algorithms/chapter_2/count_inversions.py
+++ intro_to_algorithms/chapter_2/count_inversions.py
@@ -1,44 +1,30 @@
 from typing import List
 
 
-class Incrementer:
-    def __init__(self) -> None:
-        self._value = 0
-
-    @property
-    def value(self) -> int:
-        return self._value
-
-    def increment(self):
-        self._value += 1
-
-
-def merge_in_place(arr: List[int], start: int, mid: int, end: int, inc: Incrementer) -> int:
+def merge_in_place(arr: List[int], start: int, mid: int, end: int, number_of_inversions: int) -> int:
     i = start
     while i < mid < end:
         if arr[i] > arr[mid]:
             arr[i], arr[mid] = arr[mid], arr[i]
-            inc.increment()
+            number_of_inversions += 1
         i += 1
-    return inc.value
+    return number_of_inversions
 
 
-def merge_sort_in_place_between(arr: List[int], start: int, end: int, inc: Incrementer = Incrementer()) -> int:
+def merge_sort_in_place_between(arr: List[int], start: int, end: int, number_of_inversions: int = 0) -> int:
     if end - start <= 1:
-        return inc.value
+        return number_of_inversions
     mid = int((end + start) / 2)
-    merge_sort_in_place_between(arr, 0, mid, inc)
-    merge_sort_in_place_between(arr, mid, end, inc)
-    return merge_in_place(arr, start, mid, end, inc)
+    number_of_inversions = merge_sort_in_place_between(arr, 0, mid, number_of_inversions)
+    number_of_inversions = merge_sort_in_place_between(arr, mid, end, number_of_inversions)
+    return merge_in_place(arr, start, mid, end, number_of_inversions)
 
 
-def merge_sort_in_place(arr: List[int]) -> int:
-    if not arr or len(arr) == 1:
-        return 0
-    return merge_sort_in_place_between(arr, 0, len(arr))
+def merge_sort_in_place_counting_inversions(arr: List[int]) -> int:
+    return 0 if not arr or len(arr) == 1 else merge_sort_in_place_between(arr, 0, len(arr))
 
 
 if __name__ == '__main__':
     test_arr = [5, 2, 4, 6, 1, 3]
-    print(merge_sort_in_place(test_arr))
+    print(merge_sort_in_place_counting_inversions(test_arr))
     print(test_arr)
