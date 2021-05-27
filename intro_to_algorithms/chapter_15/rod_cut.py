76e41e14d5691328fc84f29021102639014895bb
merengue <therealmerengue@gmail.com>
2021-05-27 22:25:40 +0200

    rod_cut_memo

--- intro_to_algorithms/chapter_15/rod_cut.py
+++ intro_to_algorithms/chapter_15/rod_cut.py
@@ -1,7 +1,7 @@
 from typing import List
 
 
-def rod_cut_no_dp(prices: List[int], length: int):
+def rod_cut_no_dp(prices: List[int], length: int) -> int:
     assert prices and 1 <= length <= len(prices)
     if length == 1:
         return prices[0]
@@ -11,10 +11,31 @@ def rod_cut_no_dp(prices: List[int], length: int):
     return max(revenues)
 
 
+def rod_cut_memo(prices: List[int], length: int):
+    assert prices and 1 <= length <= len(prices)
+    if length == 1:
+        return prices[0]
+
+    memo: List[int] = [-1] * len(prices)
+    memo[0] = prices[0]
+
+    def rod_cut(current_length: int) -> int:
+        if memo[current_length - 1] != -1:
+            return memo[current_length - 1]
+        revenues = [prices[current_length - 1], *[rod_cut_no_dp(prices, i) + prices[current_length - 2 - i]
+                                                  for i in range(current_length - 2, 0, -1)]]
+        max_revenue = max(revenues)
+        memo[current_length - 1] = max_revenue
+        return max_revenue
+
+    return rod_cut(length)
+
+
 def main():
-    prs = [1, 5, 8, 9, 10, 17, 17, 20, 24, 30]
-    for length in range(1, len(prs) + 1):
-        print(rod_cut_no_dp(prs, length))
+    prices = [1, 5, 8, 9, 10, 17, 17, 20, 24, 30]
+    for length in range(1, len(prices) + 1):
+        print(rod_cut_no_dp(prices, length))
+        print(rod_cut_memo(prices, length))
     pass
 
 
