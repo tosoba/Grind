de6c744f5a28308be9a10a86a1d40556b1204470
Mere Ngue <therealmerengue@gmail.com>
2021-05-29 13:49:05 +0200

    rod_cut solution fix + cut_price version

--- intro_to_algorithms/chapter_15/rod_cut.py
+++ intro_to_algorithms/chapter_15/rod_cut.py
@@ -6,12 +6,22 @@ def rod_cut_no_dp(prices: List[int], length: int) -> int:
     if length == 1:
         return prices[0]
     revenues = [prices[length - 1]]
-    for i in range(length - 2, 0, -1):
-        revenues.append(rod_cut_no_dp(prices, i) + prices[length - 2 - i])
+    for i in range(length - 2, -1, -1):
+        revenues.append(rod_cut_no_dp(prices, i + 1) + prices[length - 2 - i])
     return max(revenues)
 
 
-def rod_cut_memo(prices: List[int], length: int):
+def rod_cut_with_cut_price_no_dp(prices: List[int], length: int, cut_price: int) -> int:
+    assert prices and 1 <= length <= len(prices)
+    if length == 1:
+        return prices[0]
+    revenues = [prices[length - 1]]
+    for i in range(length - 2, -1, -1):
+        revenues.append(rod_cut_no_dp(prices, i + 1) + prices[length - 2 - i] - cut_price)
+    return max(revenues)
+
+
+def rod_cut_memo(prices: List[int], length: int) -> int:
     assert prices and 1 <= length <= len(prices)
     if length == 1:
         return prices[0]
@@ -22,8 +32,9 @@ def rod_cut_memo(prices: List[int], length: int):
     def rod_cut(current_length: int) -> int:
         if memo[current_length - 1] != -1:
             return memo[current_length - 1]
-        revenues = [prices[current_length - 1], *[rod_cut_no_dp(prices, i) + prices[current_length - 2 - i]
-                                                  for i in range(current_length - 2, 0, -1)]]
+        revenues = [prices[length - 1]]
+        for i in range(length - 2, -1, -1):
+            revenues.append(rod_cut_no_dp(prices, i + 1) + prices[length - 2 - i])
         max_revenue = max(revenues)
         memo[current_length - 1] = max_revenue
         return max_revenue
@@ -31,7 +42,7 @@ def rod_cut_memo(prices: List[int], length: int):
     return rod_cut(length)
 
 
-def rod_cut_bottom_up(prices: List[int], length: int):
+def rod_cut_bottom_up(prices: List[int], length: int) -> int:
     assert prices and 1 <= length <= len(prices)
     if length == 1:
         return prices[0]
@@ -42,8 +53,9 @@ def rod_cut_bottom_up(prices: List[int], length: int):
     def rod_cut(current_length: int):
         if memo[current_length - 1] != -1:
             return memo[current_length - 1]
-        revenues = [prices[current_length - 1], *[rod_cut_no_dp(prices, i) + prices[current_length - 2 - i]
-                                                  for i in range(current_length - 2, 0, -1)]]
+        revenues = [prices[length - 1]]
+        for i in range(length - 2, -1, -1):
+            revenues.append(rod_cut_no_dp(prices, i + 1) + prices[length - 2 - i])
         max_revenue = max(revenues)
         memo[current_length - 1] = max_revenue
 
@@ -52,12 +64,36 @@ def rod_cut_bottom_up(prices: List[int], length: int):
     return memo[-1]
 
 
+def rod_cut_with_cut_price(prices: List[int], length: int, cut_price: int) -> int:
+    assert prices and 1 <= length <= len(prices)
+    if length == 1:
+        return prices[0]
+
+    memo: List[int] = [-1] * length
+    memo[0] = prices[0]
+
+    def rod_cut(current_length: int) -> int:
+        if memo[current_length - 1] != -1:
+            return memo[current_length - 1]
+        revenues = [prices[length - 1]]
+        for i in range(length - 2, -1, -1):
+            revenues.append(rod_cut_no_dp(prices, i + 1) + prices[length - 2 - i] - cut_price)
+        max_revenue = max(revenues)
+        memo[current_length - 1] = max_revenue
+        return max_revenue
+
+    return rod_cut(length)
+
+
 def main():
     prices = [1, 5, 8, 9, 10, 17, 17, 20, 24, 30]
     for length in range(1, len(prices) + 1):
-        print(rod_cut_no_dp(prices, length))
-        print(rod_cut_memo(prices, length))
-        print(rod_cut_bottom_up(prices, length))
+        # print(rod_cut_no_dp(prices, length))
+        # print(rod_cut_memo(prices, length))
+        # print(rod_cut_bottom_up(prices, length))
+        # print(rod_cut_with_cut_price(prices, length, 1))
+        print(f'{rod_cut_memo(prices, length)} - {rod_cut_with_cut_price(prices, length, cut_price=1)}')
+
     pass
 
 
